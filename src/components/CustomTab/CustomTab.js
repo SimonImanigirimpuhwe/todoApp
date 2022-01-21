da257bd38c220837ef5bd1c7950172185c7458bc
SimonImanigirimpuhwe <softimag08@gmail.com>
2022-01-21 06:38:55 +0200

    add app template

---
+++ src/components/CustomTab/CustomTab.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import {
+    CREATE_SCREEN,
+  DASHBOARD_SCREEN,
+  EDIT_SCREEN,
+} from '../../constants/routeNames';
+import {Icon} from '../Icon';
+import {appColors} from '../../assets/theme/Colors';
+import styles from './styles';
+import DashboardScreen from '../../screens/DashboardScreen';
+import CreateScreen from '../../screens/CreateScreen';
+import EditScreen from '../../screens/EditScreen';
+
+const BottomTab = () => {
+  const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        style: {paddingBottom: 10, paddingTop: 5, height: 70},
+        labelStyle: styles.lableStyle,
+        activeTintColor: appColors.primary,
+        inactiveTintColor: appColors.secondary,
+      }}>
+      <Tab.Screen
+        name={DASHBOARD_SCREEN}
+        component={DashboardScreen}
+        options={{
+          tabBarLabel: 'HOME',
+          tabBarIcon: ({focused}) => (
+            <Icon
+              type="ionicon"
+              name="md-home-outline"
+              size={25}
+              color={focused ? appColors.primary : appColors.secondary}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Icon
+              type="material"
+              name="attach-money"
+              size={25}
+              color={focused ? appColors.primary : appColors.secondary}
+            />
+          ),
+        }}
+        name={CREATE_SCREEN}
+        component={CreateScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Icon
+              type="material"
+              name="money-off"
+              size={25}
+              color={focused ? appColors.primary : appColors.secondary}
+            />
+          ),
+        }}
+        name={EDIT_SCREEN}
+        component={EditScreen}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTab;
